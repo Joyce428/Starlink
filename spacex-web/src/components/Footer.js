bd298a33ec1f78b798e40a19e58cba22e74b94ef
Meizhen Ji <meizhenji@outlook.com>
2021-09-05 20:57:20 -0700

    second commit

---
+++ spacex-web/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                (c)2021 StarLink Tracker. All Rights Reserved. Website Made by Meizhen
+            </footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ spacex-web/src/constant.js
@@ -0,0 +1,11 @@
+export const SAT_BASE_URL = "https://api.n2yo.com/rest/v1/satellite";
+
+export const SAT_API_KEY = "DHVMP9-KQ6FZS-RLBUA6-4QUK"; //api license key
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = `${SAT_BASE_URL}/above`;
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1.0.0/world/110m.json";
+
+export  const SATELLITE_POSITION_URL = `${SAT_BASE_URL}/positions`;
