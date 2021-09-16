5ef2cce2beb56dbeb2d25696bdd0d6e94ff67fae
Meizhen Ji <meizhenji@outlook.com>
2021-09-16 12:08:24 -0700

    first commit

--- spacex-web/src/constant.js
+++ spacex-web/src/constant.js
@@ -1,6 +1,6 @@
 export const SAT_BASE_URL = "https://api.n2yo.com/rest/v1/satellite";
 
-export const SAT_API_KEY = "DHVMP9-KQ6FZS-RLBUA6-4QUK"; //api license key
+export const SAT_API_KEY = ""; //api license key
 
 export const STARLINK_CATEGORY = "52";
 
